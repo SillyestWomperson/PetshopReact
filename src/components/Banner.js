eaa609e2b9f894ceabbc2e5bbb352a748d2a418b
Luis-Tanese <luistanese@gmail.com>
2025-05-14 09:13:23 -0300

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -16,24 +16,12 @@ function Banner() {
 		<Carousel activeIndex={index} onSelect={handleSelect} className="custom-carousel">
 			<Carousel.Item>
 				<img src={banner} className="d-block w-100" alt="womp"></img>
-				<Carousel.Caption>
-					<h3>First slide label</h3>
-					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item>
 				<img src={banner01} className="d-block w-100" alt="womp"></img>
-				<Carousel.Caption>
-					<h3>Second slide label</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item>
 				<img src={banhoETosa} className="d-block w-100" alt="womp"></img>
-				<Carousel.Caption>
-					<h3>Third slide label</h3>
-					<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-				</Carousel.Caption>
 			</Carousel.Item>
 		</Carousel>
 	);
